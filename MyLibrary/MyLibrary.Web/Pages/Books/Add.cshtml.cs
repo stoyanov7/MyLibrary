0ac5740115bc88fa71e628650151193c5ba8cc13
Georgi Stoyanov <georgi.stoyanov777@gmail.com>
2019-01-15 11:26:40 +0200

    Implement add and details for books

---
+++ MyLibrary/MyLibrary.Web/Pages/Books/Add.cshtml.cs
@@ -0,0 +1,65 @@
+﻿namespace MyLibrary.Web.Pages.Books
+{
+    using System.ComponentModel.DataAnnotations;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using Data;
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.AspNetCore.Mvc.RazorPages;
+    using Microsoft.EntityFrameworkCore;
+    using Models;
+
+    public class AddModel : PageModel
+    {
+        private readonly MyLibraryContext context;
+
+        public AddModel(MyLibraryContext context)
+        {
+            this.context = context;
+        }
+
+        [BindProperty]
+        public string Title { get; set; }
+
+        [BindProperty]
+        public string Description { get; set; }
+
+        [BindProperty]
+        public string Author { get; set; }
+
+        [BindProperty]
+        [Display(Name = "Image URL")]
+        [DataType(DataType.ImageUrl)]
+        public string ImageUrl { get; set; }
+
+        public async Task<IActionResult> OnPost()
+        {
+            if (this.ModelState.IsValid)
+            {
+                var book = new Book { Title = this.Title };
+
+                var author = await this.context
+                    .Authors
+                    .FirstOrDefaultAsync(a => a.Name == this.Author);
+
+                if (author is null)
+                {
+                    author = new Author { Name = this.Author };
+                    this.context.Authors.Add(author);
+                    await this.context.SaveChangesAsync();
+                }
+
+                book.AuthorId = author.Id;
+                book.ImageUrl = this.ImageUrl;
+                book.Description = this.Description;
+
+                this.context.Books.Add(book);
+                await this.context.SaveChangesAsync();
+
+                return this.RedirectToPage("/Books/Details", new { id = book.Id});
+            }
+
+            return this.Page();
+        }
+    }
+}
---
+++ MyLibrary/MyLibrary.Web/Pages/Books/Details.cshtml.cs
@@ -0,0 +1,47 @@
+﻿namespace MyLibrary.Web.Pages.Books
+{
+    using System.Linq;
+    using System.Threading.Tasks;
+    using Data;
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.AspNetCore.Mvc.RazorPages;
+    using Microsoft.EntityFrameworkCore;
+
+    public class DetailsModel : PageModel
+    {
+        private readonly MyLibraryContext context;
+
+        public DetailsModel(MyLibraryContext context)
+        {
+            this.context = context;
+        }
+
+        public string Title { get; set; }
+
+        public string ImageUrl { get; set; }
+
+        public string Description { get; set; }
+
+        public string Author { get; set; }
+
+        public async Task<IActionResult> OnGet(int id)
+        {
+            var book = await this.context
+                .Books
+                .Include(b => b.Author)
+                .FirstOrDefaultAsync(b => b.Id == id);
+
+            if (book == null)
+            {
+                return this.NotFound();
+            }
+
+            this.Title = book.Title;
+            this.Description = book.Description;
+            this.ImageUrl = book.ImageUrl;
+            this.Author = book.Author.Name;
+
+            return this.Page();
+        }
+    }
+}
